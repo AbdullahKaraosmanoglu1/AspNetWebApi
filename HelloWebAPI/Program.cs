84bda7ea2e685fa0f8ff56a71a1d73f13aecf7fa
AbdullahKaraosmanoglu <a.k.oglu4153@gmail.com>
2024-04-18 15:28:51 +0300

    Use Swagger Packages and process of defining swagger into the required Program.cs file.

--- HelloWebAPI/Program.cs
+++ HelloWebAPI/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Service (Container)
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
