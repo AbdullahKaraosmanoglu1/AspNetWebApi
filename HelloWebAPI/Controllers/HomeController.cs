e9a01487311a12905858d77ac7c518bd3f4488a9
AbdullahKaraosmanoglu <a.k.oglu4153@gmail.com>
2024-04-18 15:36:08 +0300

    Controllers infrastructure has been established.

---
+++ HelloWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public String GetMessage()
+        {
+            return "S.A Dost";
+        }
+    }
+}
--- HelloWebAPI/Program.cs
+++ HelloWebAPI/Program.cs
@@ -9,5 +9,6 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+app.MapControllers();
 
 app.Run();
