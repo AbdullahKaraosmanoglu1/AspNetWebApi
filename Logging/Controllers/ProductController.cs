f9e95ccbc31196d5a25355aaf12adbbec235faf6
AbdullahKaraosmanoglu <a.k.oglu4153@gmail.com>
2024-04-19 14:34:56 +0300

    Build-in logging providers. *Dependency Injection.

---
+++ Logging/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Logging.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logging.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var products = new List<ProductModel>()
+            {
+                new ProductModel { Id = 1,Name="Computer"},
+                new ProductModel { Id = 2,Name="Keyboard"},
+                new ProductModel { Id = 3,Name="Mouse"}
+            };
+
+            _logger.LogInformation("GetAllProducts action has been called");
+
+            return Ok(products);
+        }
+    }
+}
---
+++ Logging/Models/ProductModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Logging.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
