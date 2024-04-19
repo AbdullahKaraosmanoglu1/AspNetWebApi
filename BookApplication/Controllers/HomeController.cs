42461f94e9c923e01f1c2b71da2f15a9fea5f5cf
AbdullahKaraosmanoglu <a.k.oglu4153@gmail.com>
2024-04-19 16:34:56 +0300

    Created Book Application Project.

---
+++ BookApplication/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+    }
+}
