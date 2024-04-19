97a2d6b7933015f563945b02b582ea6b5d50c870
AbdullahKaraosmanoglu <a.k.oglu4153@gmail.com>
2024-04-19 17:11:36 +0300

    Application Context.

--- BookApplication/Controllers/HomeController.cs
+++ BookApplication/Controllers/HomeController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BookApplication.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookApplication.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Books")]
     [ApiController]
     public class HomeController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContext.BooksList;
 
+            return Ok(books);
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBooks([FromRoute(Name = "id")] int id)
+        {
+            var book = ApplicationContext
+                .BooksList
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            if (book is null)
+                return NotFound();
+
+            return Ok(book);
+        }
     }
 }
---
+++ BookApplication/Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using BookApplication.Models;
+
+namespace BookApplication.Data
+{
+    public class ApplicationContext
+    {
+        public static List<BookModel> BooksList { get; set; }
+        static ApplicationContext()
+        {
+            BooksList = new List<BookModel>()
+            {
+                new BookModel() {Id=1, Title="Beyaz Diş", Price=100},
+                new BookModel() {Id=2, Title="Gazap Üzümleri", Price=200},
+                new BookModel() {Id=3, Title="Genç Verterin Acıları", Price=300}
+            };
+        }
+    }
+}
