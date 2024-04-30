8e349ef97f2049aa3283681d5df208f603c87920
AbdullahKaraosmanoglu <a.k.oglu4153@gmail.com>
2024-04-30 17:00:06 +0300

    * Necessary Properties were added to the User class

--- BookApplication.Data/Entity/User.cs
+++ BookApplication.Data/Entity/User.cs
@@ -9,7 +9,16 @@ namespace BookApplication.Data.Entity
         [StringLength(100)]
         public string LastName { get; set; }
         [StringLength(500)]
+        public string Email { get; set; }
+        [StringLength(500)]
+        public string Password { get; set; }
+        public string? AccessToken { get; set; }
+        public DateTime? AccessTokenCreatedDate { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenCreatedDate { get; set; }
+        [StringLength(100)]
         public string? ImagePath { get; set; }
+        public decimal MoneyAmount { get; set; }
 
         public ICollection<UserBook> UserBooks { get; set; }
 
