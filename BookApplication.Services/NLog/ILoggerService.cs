6bffd5ba780a526cd6a4422d54650ca6acfd3398
AbdullahKaraosmanoglu <a.k.oglu4153@gmail.com>
2024-05-02 16:02:13 +0300

    * Added "NLog.Extensions.Logging" Package Project to BookApplication.Service. * Create ILoggerService interface class.

---
+++ BookApplication.Services/NLog/ILoggerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApplication.Services.NLog
+{
+    public interface ILoggerService
+    {
+        Task LogInfo(string message);
+        Task LogWarning(string message);
+        Task LogError(string message);
+        Task LogDebug(string message);
+    }
+}
