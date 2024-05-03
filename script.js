d28d4643341f52bf80062a1a95f1a83729676f4c
AbdullahKaraosmanoglu <a.k.oglu4153@gmail.com>
2024-05-03 15:32:25 +0300

    * The basic files for sending requests to EndPoints were created using jQuery and Ajax.

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+
+/*GETALL Request*/
 $(document).ready(function () {
     $('#getDataBtn').click(function () {
         $.ajax({
             url: "https://localhost:7238/api/Book",
             method: 'GET',
             success: function (data) {
-                console.log(data.data);
-                for (var i = 0; i < data.data.length; i++) {
-                    var item = data.data[i];
-                    console.log(item);
-                    $('.name').html(item.name);
-                    $('.author').html(item.author);
-                    $('.publisher').html(item.publisher);
-                }
+                console.log(data);
+                var booksHtml = ''; // Kitaplarýn HTML içeriðini saklayacak deðiþken
+                data.data.forEach(function (book) {
+                    booksHtml += '<div class="book">';
+                    booksHtml += '<p class="name">Name: ' + book.name + '</p>';
+                    booksHtml += '<p class="author">Author: ' + book.author + '</p>';
+                    booksHtml += '<p class="publisher">Publisher: ' + book.publisher + '</p>';
+                    // Diðer özellikleri de ekleyebilirsiniz
+                    booksHtml += '</div>';
+                });
+                $('#dataContainer').html(booksHtml); // Kitaplarýn HTML içeriðini dataContainer içine ekle
             },
             error: function (error) {
                 console.error(error);
