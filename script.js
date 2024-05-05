ccd2168f3f99964927be5682274bd367c51c207f
Sinan Can <Sinan51can@gmail.com>
2024-05-05 11:39:56 +0300

    jquery,css ve html sayfalarında düzenlemeler yapıldı.

--- script.js
+++ script.js
@@ -1,130 +1,89 @@
-﻿/* GetAllAsync(); EndPoint Request*/
+﻿/* GetAll EndPoint Request*/
+
 $(document).ready(function () {
-    $('#getAllBooksBtn').click(function () {
+    $('#getData').click(function () {
         $.ajax({
             url: "https://localhost:7238/api/Book",
             method: 'GET',
             success: function (data) {
                 console.log(data);
-                var booksHtml = ''; // Kitapların HTML içeriğini saklayacak değişken
+                var tableContent = ''; // Tablo içeriðini saklayacak deðiþken
                 data.data.forEach(function (book) {
-                    booksHtml += '<div class="book">';
-                    booksHtml += '<p class="name">Name: ' + book.name + '</p>';
-                    booksHtml += '<p class="author">Author: ' + book.author + '</p>';
-                    booksHtml += '<p class="publisher">Publisher: ' + book.publisher + '</p>';
-                    // Diğer özelliklerde isteğe göre eklenebilir...
-                    booksHtml += '</div>';
+                    tableContent += '<tr>';
+                    tableContent += '<td>' + book.id + '</td>';
+                    tableContent += '<td>' + book.name + '</td>';
+                    tableContent += '<td>' + book.author + '</td>';
+                    tableContent += '<td>' + book.language + '</td>';
+                    tableContent += '<td>' + book.publisher + '</td>';
+                    tableContent += '<td>' + book.imagePath + '</td>';
+                    tableContent += '<td>' + book.slugName + '</td>';
+                    tableContent += '<td>' + book.printingYear + '</td>';
+                    tableContent += '<td>' + book.pageCount + '</td>';
+                    tableContent += '<td>' + book.price + '</td>';
+                    tableContent += '<td>' + book.bookCategoryId + '</td>';
+                    tableContent += '<td>' + book.createdDate + '</td>';
+                    tableContent += '<td>' + book.updatedDate + '</td>';
+                    tableContent += '<td>' + book.isActive + '</td>';
+                    tableContent += '<td>' + book.isDeleted + '</td>';
+                    tableContent += '<td>'; // Ýþlemler için yeni bir hücre
+                    tableContent += '<i class="fas fa-edit edit-icon" data-book-id="' + book.id + '"></i>'; // Güncelleme ikonu
+                    tableContent += '<i class="fas fa-trash delete-icon" data-book-id="' + book.id + '"></i>'; // Silme ikonu
+                    tableContent += '<i class="fas fa-info-circle info-icon" data-book-id="' + book.id + '"></i>'; // Detay ikonu
+                    tableContent += '</td>';
+                    tableContent += '</tr>';
                 });
-                $('#dataContainer').html(booksHtml); // Kitapların HTML içeriğini dataContainer içine ekle
-            },
-            error: function (error) {
-                console.error(error);
+                $('#dataBody').html(tableContent); // Tablo içeriðini dataBody içine ekle
             }
         });
     });
-});
 
-/* GetByIdAsync(); EndPoint Request */
-$(document).ready(function () {
-    $('#getBookByIdSection').click(function () {
-        // Örnek olarak, alınacak kitabın ID'si #bookIdInput alanından alınacak
-        var bookId = $('#bookIdInput').val();
 
-        // AJAX çağrısı yapılıyor
-        $.ajax({
-            url: "https://localhost:7238/api/Book/" + bookId, // Book ID'sini URL'ye ekleyerek istek yapılıyor
-            method: 'GET',
-            success: function (data) {
-                console.log(data);
+    /* GetByIdAsync(); EndPoint Request */
+    $(document).ready(function () {
+        // info iconuna tıklandığında
+        $(document).on('click', '.info-icon', function () {
+            var bookId = $(this).data('book-id');
 
-                // Alınan kitabın bilgileri HTML içeriğine dönüştürülüyor
-                var bookHtml = '<div class="book">';
-                bookHtml += '<p class="name">Name: ' + data.data.name + '</p>';
-                bookHtml += '<p class="author">Author: ' + data.data.author + '</p>';
-                bookHtml += '<p class="publisher">Publisher: ' + data.data.publisher + '</p>';
-                // Diğer özelliklerde isteğe göre eklenebilir...
-                bookHtml += '</div>';
-
-                // Kitap HTML içeriği dataContainer içine ekleniyor
-                $('#dataContainer').html(bookHtml);
-            },
-            error: function (error) {
-                console.error(error);
-            }
+            // Fetch book data from the API
+            $.ajax({
+                url: "https://localhost:7238/api/Book/" + bookId,
+                method: 'GET',
+                success: function (data) {
+                    // Verileri Local Storage'a kaydet
+                    localStorage.setItem('bookData', JSON.stringify(data.data));
+                    // Diğer sayfaya yönlendir
+                    window.location.href = 'GetById.html';
+                },
+                error: function (xhr, status, error) {
+                    console.error(xhr.responseText);
+                }
+            });
         });
+    });
+
+    $(document).ready(function () {
+        // Local Storage'dan verileri al
+        var bookData = JSON.parse(localStorage.getItem('bookData'));
+        if (bookData) {
+            // Verileri inputlara yerleştir
+            $('#id').val(bookData.id);
+            $('#updatedName').val(bookData.name);
+            $('#updatedAuthor').val(bookData.author);
+            $('#updatedLanguage').val(bookData.language);
+            $('#updatedPublisher').val(bookData.publisher);
+            $('#updatedImagePath').val(bookData.imagePath);
+            $('#updatedSlugName').val(bookData.slugName);
+            $('#updatedPrintingYear').val(bookData.printingYear);
+            $('#updatedPageCount').val(bookData.pageCount);
+            $('#updatedPrice').val(bookData.price);
+            $('#updatedBookCategoryId').val(bookData.bookCategoryId);
+            $('#updatedCreatedDate').val(bookData.createdDate);
+            $('#updatedUpdatedDate').val(bookData.updatedDate);
+            $('#updatedIsActive').val(bookData.isActive);
+            $('#updatedIsDeleted').val(bookData.isDeleted);
+
+            // Local Storage'dan verileri temizle
+        }
     });
 });
 
-/* CreateBookAsync(); EndPoint Request */
-$(document).ready(function () {   
-
-    // Yeni kitap oluştur butonuna tıklandığında
-    $('#createBookBtn').click(function () {
-        // Kullanıcının girdiği bilgileri al
-        var name = $('#bookNameInput').val();
-        var author = $('#authorInput').val();
-        var language = $('#languageInput').val();
-        var publisher = $('#publisherInput').val();
-        var imagePath = $('#imagePathInput').val();
-        var printingYear = parseInt($('#printingYearInput').val());
-        var pageCount = parseInt($('#pageCountInput').val());
-        var price = parseFloat($('#priceInput').val());
-        var bookCategoryId = parseInt($('#bookCategoryIdInput').val());
-
-        // Yeni kitap nesnesi oluştur
-        var newBook = {
-            Name: name,
-            Author: author,
-            Language: language,
-            Publisher: publisher,
-            ImagePath: imagePath,
-            SlugName: name,
-            PrintingYear: printingYear,
-            PageCount: pageCount,
-            Price: price,
-            BookCategoryId: bookCategoryId
-        };
-
-        // AJAX isteği gönder
-        $.ajax({
-            url: "https://localhost:7238/api/Book",
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(newBook),
-            success: function (data) {
-                console.log("Yeni kitap oluşturuldu:", data);
-                // Başarılı bir şekilde oluşturulan kitabı göstermek için gerekli kodu buraya ekleyin
-            },
-            error: function (error) {
-                console.error("Yeni kitap oluşturulurken bir hata oluştu:", error);
-                // Hata durumunda kullanıcıya bir hata mesajı göstermek için gerekli kodu buraya ekleyin
-            }
-        });
-    });
-});
-
-/* DeleteAsync(); EndPoint Request */
-$(document).on("click", ".delete-icon", function () {
-    // Silinecek kitavın ID'sini al
-    var bookId = $(this).data("book-id");
-
-    // Confirm dialog ile kullanıcıdan silme işleminin onay al
-    var confirmation = confirm("Bu kitabı silmek istediðinize emin misiniz?");
-
-    // Kullanıcı onaylarsa
-    if (confirmation) {
-        // AJAX isteði göndererek kitabı sil
-        $.ajax({
-            url: "https://localhost:7238/api/Book/" + bookId,
-            type: "DELETE",
-            success: function (response) {
-                // Başarılı yanıt aldıðımızda veriyi yeniden yükle
-                $("#getData").click();
-            },
-            error: function (xhr, status, error) {
-                // Hata durumunda konsola hata mesajını yazdır
-                console.error(xhr.responseText);
-            }
-        });
-    }
-});
