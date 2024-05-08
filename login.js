96cad75f249746e06024b6edacbb850f7250f125
AbdullahKaraosmanoglu <a.k.oglu4153@gmail.com>
2024-05-08 13:23:21 +0300

    * The functions in the script files were written separately and called.

--- login.js
+++ login.js
@@ -1,29 +1,30 @@
-// JavaScript source code
+function loginUser() {
+    var userLogin = {
+        email: $('#emailInput').val(),
+        password: $('#passwordInput').val()
+    };
+
+    $.ajax({
+        url: "https://localhost:7238/api/Login/UserLogin",
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(userLogin),
+
+        success: function (response) {
+            console.log('Giriþ Baþarýlý');
+            localStorage.setItem('token', response.data);
+            console.log(response);
+            window.location.href = 'index.html';
+        },
+        error: function (xhr, status, error) {
+            console.error(xhr.responseText);
+        }
+    });
+}
+
 $(document).ready(function () {
     $('#loginBtn').click(function (event) {
         event.preventDefault();
-
-        var userLogin = {
-            email: $('#emailInput').val(),
-            password: $('#passwordInput').val()
-        };
-
-        $.ajax({
-            url: "https://localhost:7238/api/Login/UserLogin",
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(userLogin),
-
-            success: function (response) {
-                console.log('Giriþ Baþarýlý');
-                localStorage.setItem('token', response.data);
-                console.log(response);
-                window.location.href = 'index.html';
-            },
-            error: function (xhr, status, error) {
-                console.error(xhr.responseText);
-            }
-        });
+        loginUser();
     });
 });
-
